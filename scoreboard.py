143f5d2efdf4c14aeb74ff99af1f5b3481c7139a
Pushkaltoocool <118830976+Pushkaltoocool@users.noreply.github.com>
2025-01-27 16:31:53 +0800

    added high score mechanism

--- scoreboard.py
+++ scoreboard.py
@@ -1,22 +1,38 @@
 from turtle import Turtle
-import time
+
+with open("high_score.txt") as high_score_file:
+    high_score = int(high_score_file.read())
+
+
+highscoredoc = open("high_score.txt")
+highscore = int(highscoredoc.read())
+highscoredoc.close()
 
 class Scoreboard(Turtle):
-    def __init__(self, position):
+    def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = highscore
         self.color("white")
         self.penup()
+        self.goto(0, 260)
         self.hideturtle()
-        self.goto(position)
-    
-    def update_score(self):
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"{self.score}", font=("Courier", 50, "normal"))
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", align="center", font=("Courier", 24, "normal"))
 
-    def game_over(self, winner):    
-        self.goto(0, 0)
-        self.write("GAME OVER", font=("Courier", 50, "normal"), align="center")
-        self.goto(0, -50)
-        self.write(f"{winner} wins!", font=("Courier", 50, "normal"), align="center")
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.clear()
+        self.update_scoreboard()
+        with open("high_score.txt", mode="w") as high_score_file:
+            high_score_file.write(f"{self.high_score}")
